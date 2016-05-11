68fcc7aa3b7aa312e21ea38cd49d0e293005e440
shadd0w <rene.gmitter@wame.sk>
2016-05-11 16:12:04 +0200

    Add show action to Gallery grid

---
+++ grids/ShowGridAction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wame\DataGridControl;
+
+class ShowGridAction extends BaseGridColumn
+{
+	public function addColumn($grid)
+	{
+		$grid->addAction('show', '', ":{$grid->parent->presenterName}:show")
+			->setIcon('eye')
+			->setTitle('Show')
+			->setClass('btn btn-xs btn-default ajax');
+		
+		return $grid;
+	}
+}
