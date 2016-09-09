7e68300b1028ba196a3cbce452728a2f169011ad
shadd0w <rene.gmitter@wame.sk>
2016-09-09 09:41:44 +0200

    UPDATE DataGridControl

--- repositories/GridRepository.php
+++ repositories/GridRepository.php
@@ -2,24 +2,14 @@
 
 namespace Wame\DataGridControl\Repositories;
 
+use Wame\Core\Repositories\BaseRepository;
 use Wame\DataGridControl\Entities\GridEntity;
-use Wame\UserModule\Entities\UserEntity;
-use Wame\Core\Exception\RepositoryException;
 
-class GridRepository extends \Wame\Core\Repositories\BaseRepository
+class GridRepository extends BaseRepository
 {
-	/** @var UserEntity */
-	private $userEntity;
-	
-	public function __construct(
-		\Nette\DI\Container $container, 
-		\Kdyby\Doctrine\EntityManager $entityManager, 
-		\h4kuna\Gettext\GettextSetup $translator, 
-		\Nette\Security\User $user
-	) {
-		parent::__construct($container, $entityManager, $translator, $user, GridEntity::CLASS);
-		
-		$this->userEntity = $this->entityManager->getRepository(UserEntity::class)->findOneBy(['id' => $user->id]);
+	public function __construct()
+    {
+		parent::__construct(GridEntity::CLASS);
 	}
 	
 }
