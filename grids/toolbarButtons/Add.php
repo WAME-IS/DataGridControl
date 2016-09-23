ed61f3f9fe2c647eea8836e265d716c3f3ca36d5
Stanislav Červeňák <stano@wame.sk>
2016-09-23 12:27:18 +0200

    EDIT GRIDS add buttons

--- grids/toolbarButtons/Add.php
+++ grids/toolbarButtons/Add.php
@@ -6,12 +6,98 @@ use Wame\DataGridControl\BaseGridItem;
 
 class Add extends BaseGridItem
 {
+    /** @var string */
+    protected $class = 'btn btn-success';
+    
+    /** @var string */
+    protected $icon = 'fa fa-plus';
+    
+    /** @var string */
+    private $link;
+    
+    /** @var string */
+    private $title;
+    
+    
+    /** set **********************************************************************/
+
+
+    public function setClass($class)
+    {
+        $this->class = $class;
+        
+        return $this;
+    }
+
+
+    public function setIcon($icon)
+    {
+        $this->icon = $icon;
+        
+        return $this;
+    }
+
+
+    public function setLink($link)
+    {
+        $this->link = $link;
+        
+        return $this;
+    }
+    
+    
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        
+        return $this;
+    }
+    
+    
+    /** get **********************************************************************/
+
+    
+    private function getClass()
+    {
+        return $this->class;
+    }
+    
+    
+    private function getIcon()
+    {
+        return $this->icon;
+    }
+    
+    
+    private function getLink($grid)
+    {
+        if ($this->link) {
+            return $this->link;
+        } else {
+            return ":{$grid->presenter->getName()}:create";
+        }
+    }
+    
+    
+    private function getTitle()
+    {
+        if ($this->title) {
+            return $this->title;
+        } else {
+            return _('Add');
+        }
+    }
+
+    
+    /** render **********************************************************************/
+
+
     /** {@inheritDoc} */
 	public function render($grid)
     {
-        $grid->addToolbarButton(":{$grid->presenter->getName()}:create", _('Add'))
-                ->setIcon('fa fa-plus')
-                ->setClass('btn btn-success');
+        $grid->addToolbarButton($this->getLink($grid), $this->getTitle())
+                ->setIcon($this->getIcon())
+                ->setClass($this->getClass());
                 
 		return $grid;
 	}
