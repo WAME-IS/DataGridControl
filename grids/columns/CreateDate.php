c487958499a39cbc265bd97237a574aa3943c05f
Stanislav Červeňák <stano@wame.sk>
2016-12-01 09:39:05 +0100

    EDIT DataGrid date columns

--- grids/columns/CreateDate.php
+++ grids/columns/CreateDate.php
@@ -2,16 +2,28 @@
 
 namespace Wame\DataGridControl\Columns;
 
+use Nette\Utils\Html;
 use Wame\DataGridControl\BaseGridItem;
+use Wame\Utils\Date;
+
 
 class CreateDate extends BaseGridItem
 {
     /** {@inheritDoc} */
-	public function render($grid) {
+	public function render($grid)
+    {
 		$grid->addColumnDateTime('createDate', _('Created'))
-				->setFilterDate();
-		
+                ->setFormat('d.m.Y H:i')
+                ->setRenderer(function($user) {
+                    $date = $user->getCreateDate();
+
+                    return Html::el('div')->setText(Date::toString($date, 'd.m.Y')) . Html::el('small')->setText(Date::toString($date, 'H:i:s'));
+                })
+                ->setTemplateEscaping(false)
+                ->setSortable()
+                ->setFilterDate();
+
 		return $grid;
 	}
-    
+
 }
--- grids/columns/Published.php
+++ grids/columns/Published.php
@@ -2,21 +2,40 @@
 
 namespace Wame\DataGridControl\Columns;
 
+use Nette\Utils\Html;
 use Wame\DataGridControl\BaseGridItem;
+use Wame\Utils\Date;
+
 
 class Published extends BaseGridItem
 {
     /** {@inheritDoc} */
-	public function render($grid) {
+	public function render($grid)
+    {
 		$grid->addColumnDateTime('publishedBy', _('Published by'), 'publishStartDate')
-//				->setSortable()
+                ->setFormat('d.m.Y H:i')
+                ->setRenderer(function($user) {
+                    $date = $user->getPublishStartDate();
+
+                    return Html::el('div')->setText(Date::toString($date, 'd.m.Y')) . Html::el('small')->setText(Date::toString($date, 'H:i:s'));
+                })
+                ->setTemplateEscaping(false)
+				->setSortable()
 				->setFilterDate();
-		
+
+
 		$grid->addColumnDateTime('publishedAt', _('Published at'), 'publishEndDate')
-//				->setSortable()
+                ->setFormat('d.m.Y H:i')
+                ->setRenderer(function($user) {
+                    $date = $user->getPublishEndDate();
+
+                    return Html::el('div')->setText(Date::toString($date, 'd.m.Y')) . Html::el('small')->setText(Date::toString($date, 'H:i:s'));
+                })
+                ->setTemplateEscaping(false)
+				->setSortable()
 				->setFilterDate();
-		
+
 		return $grid;
 	}
-    
+
 }
